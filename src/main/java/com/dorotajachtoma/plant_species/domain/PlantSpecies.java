d97c55b043086660518208194474d8d8749432bd
dorotajachtoma <jachtoma.do@gmail.com>
2026-05-18 19:37:31 +0200

    #3 - add plantSpecies entity

---
+++ src/main/java/com/dorotajachtoma/plant_species/domain/PlantSpecies.java
@@ -0,0 +1,46 @@
+package com.dorotajachtoma.plant_species.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "PLANT_SPECIES")
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlantSpecies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT", nullable = true)
+    private LocalDateTime updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
