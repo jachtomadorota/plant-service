458202bd3a851728bae69906c733f514b4bd5a3d
dorotajachtoma <jachtoma.do@gmail.com>
2026-05-25 20:10:34 +0200

    #3 - add Collection entity & adjust Household entity

--- src/main/java/com/dorotajachtoma/collection/domain/Collection.java
+++ src/main/java/com/dorotajachtoma/collection/domain/Collection.java
@@ -1,13 +1,22 @@
 package com.dorotajachtoma.collection.domain;
 
+import com.dorotajachtoma.owner.domain.Owner;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -19,4 +28,28 @@ public class Collection {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS", nullable = false)
+    private String status;
+
+    @OneToMany
+    @JoinTable(joinColumns = @JoinColumn(name = "OWNER_ID", referencedColumnName = "ID"))
+    private Owner owner;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+
+    public void setStatus(String status) {
+        this.status = CollectionStatus.ACTIVE.name();
+    }
 }
